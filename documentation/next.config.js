3937fc849a11c3e07a31e619875ef2f5ce816856
sohantalukder <sohan15-1933@diu.edu.bd>
2025-07-02 01:25:18 +0600

    Refactor project structure and update configuration files for improved organization and clarity.

--- documentation/next.config.js
+++ documentation/next.config.js
@@ -1,14 +1,15 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production'
+const isExport = process.env.NEXT_EXPORT === 'true'
 
 const nextConfig = {
-  output: 'export',
+  output: isProd ? 'export' : undefined,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
-  assetPrefix: isProd ? '/react-native-boilerplate' : '',
-  basePath: isProd ? '/react-native-boilerplate' : '',
+  assetPrefix: isProd && isExport ? '/react-native-boilerplate' : '',
+  basePath: isProd && isExport ? '/react-native-boilerplate' : '',
 }
 
 module.exports = nextConfig 
